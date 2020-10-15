359e0ee6d01835c6a28e5d7e1da252cfe7e90eea
Wally <Wally@Wallys-MBP.home>
2020-10-15 10:27:27 -0400

    added operator.js

---
+++ operators.js
@@ -0,0 +1,18 @@
+var a = 5
+a++
+a=a+1
+++a
+a=+a
+a=-a
+console.log(a)
+var b=7
+b--
+--b
+b=b-1
+console.log(b)
+//logical operator && || !
+var c=true
+var d=true
+console.log(c && d)
+console.log(c || d)
+console.log(!d)
